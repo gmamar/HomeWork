fa4eba48bcc6e172ca3a39ee1896fc30f21761d9
gmamar <gmamar1@hotmail.com>
2017-05-24 06:33:48 +0200

    update xhr request Function

--- Week7/main.js
+++ Week7/main.js
@@ -1,53 +1,69 @@
 "use strict";
 var avatar = document.getElementById("avatar");
+var rName = document.getElementById("repoName");
 var userName = document.getElementById("uName");
 var pRepo = document.getElementById("pRepo");
 var sBTN = document.getElementById("entrBTN");
 var pInfo = document.getElementById("allInfo");
+var srcEntry = document.getElementById("srcName");
 var profileInformation = ["id", "gravatar_id", "url", "html_url", "followers_url", "following_url", "gists_url",
-						"starred_url", "subscriptions_url", "organizations_url", "repos_url", "events_url", "received_events_url",
+				   		"starred_url", "subscriptions_url", "organizations_url", "repos_url", "events_url", "received_events_url",
 						 "type", "site_admin", "name", "company", "blog", "location", "email", "hireable", "bio", "public_repos",
 						 "public_gists", "followers", "following", "created_at", "updated_at"];
 
-function gitProfille(){
-	var srcEntry = document.getElementById("srcName").value;
-	var link = 'https://api.github.com/users/' + srcEntry;
+function gitProfille() {
+
+
+	var link = 'https://api.github.com/users/' + srcEntry.value;
 	mainRequest(link);
 }
 
-function repoPage(){
-	var repoPageLink = 'https://api.github.com/users/' + "gmamar" + '/repos';
-	console.log(repoPageLink);
+function repoPage() {
+
+	var repoPageLink = 'https://api.github.com/users/' + srcEntry.value + '/repos';
 	mainRequest(repoPageLink);
+}
+
+function renderRepo(repoinf) {
+	function repoNameList(index, item) {
+		var lil = document.createElement("li");
+		console.log(repoinf[item].name);
+		lil.innerHTML = repoinf[item].name;
+		console.log(lil);
+		rName.appendChild(lil);
+	}
+	repoinf.forEach(repoNameList);
+
 }
 
 function mainRequest(targetPage) {
 	function processRequest() {
 		if (xhr.readyState == 4) {
-			console.log(xhr.response);
-			renderInfo(xhr.response);
+			var parsedInfo = JSON.parse(xhr.response);
+			if (parsedInfo.login) {
+				renderInfo(parsedInfo);
+			} else if (parsedInfo[0].archive_url) {
+				renderRepo(parsedInfo);
+			}
 		}
 	}
-	
+
 	var requestURL = targetPage;
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', requestURL, true);
 	xhr.send();
 	xhr.onreadystatechange = processRequest;
-	console.log(xhr.onreadystatechange);
 }
 
-function renderInfo(info) {
-	var parsedInfo = JSON.parse(info);
-	userName.innerHTML = "User name: " + parsedInfo.login;;
-	pRepo.innerHTML = "Public repos: " + parsedInfo.public_repos;
-	avatar.setAttribute("src", parsedInfo.avatar_url); //	load User avatr
+function renderInfo(Info) {
+	console.log(Info);
+	userName.innerHTML = "User name: " + Info.login;;
+	pRepo.innerHTML = "Public repos: " + Info.public_repos;
+	avatar.setAttribute("src", Info.avatar_url); //	load User avatr
 	pInfo.innerHTML = "";
-	addFulllInfo(parsedInfo);
+	addFulllInfo(Info);
 
 	function addFulllInfo(userData) { // load user information
-
-
 		for (var i = 0; i < profileInformation.length; i++) {
 			var li = document.createElement("li");
 			li.innerHTML = profileInformation[i] + ": " + userData[profileInformation[i]];
@@ -60,17 +76,17 @@ function renderInfo(info) {
 	}
 
 	function openGit() {
-		window.open(parsedInfo[profileInformation[3]]);
+		window.open(Info[profileInformation[3]]);
 	}
 
 	userName.addEventListener("click", showInfo);
 	avatar.addEventListener("click", openGit);
-//	pRepo.addEventListener("click", repoPage);
+	pRepo.addEventListener("click", repoPage);
 
 }
 
-function repo(){
-	
+function repo() {
+
 }
 
 
